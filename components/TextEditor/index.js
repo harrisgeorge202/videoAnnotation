f1ab0aaaffe1dc36415d37e776c8a7a9b835fbbf
Nimisha <nimisha@toobler.com>
2019-08-27 11:26:19 +0530

    New changes 3

--- components/TextEditor/index.js
+++ components/TextEditor/index.js
@@ -2,10 +2,11 @@
 
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import { Input, Button, Avatar } from "antd";
+// import { Input, Button, Avatar } from "antd";
 
 var _templateObject = _taggedTemplateLiteralLoose(['\n  padding: 8px 16px;\n\n  textarea {\n    border: 0;\n    font-size: 14px;\n    margin: 6px 0;\n    min-height: 60px;\n    outline: 0;\n  }\n'], ['\n  padding: 8px 16px;\n\n  textarea {\n    border: 0;\n    font-size: 14px;\n    margin: 6px 0;\n    min-height: 60px;\n    outline: 0;\n  }\n']),
-    _templateObject2 = _taggedTemplateLiteralLoose(['\n  background: whitesmoke;\n  border: 0;\n  box-sizing: border-box;\n  color: #363636;\n  cursor: pointer;\n  font-size: 1rem;\n  margin: 0;\n  outline: 0;\n  padding: 8px 16px;\n  text-align: center;\n  text-shadow: 0 1px 0 rgba(0,0,0,0.1);\n  width: 100%;\n\n  transition: background 0.21s ease-in-out;\n\n  &:focus, &:hover {\n    background: #eeeeee;\n  }\n'], ['\n  background: whitesmoke;\n  border: 0;\n  box-sizing: border-box;\n  color: #363636;\n  cursor: pointer;\n  font-size: 1rem;\n  margin: 0;\n  outline: 0;\n  padding: 8px 16px;\n  text-align: center;\n  text-shadow: 0 1px 0 rgba(0,0,0,0.1);\n  width: 100%;\n\n  transition: background 0.21s ease-in-out;\n\n  &:focus, &:hover {\n    background: #eeeeee;\n  }\n']);
+    _templateObject2 = _taggedTemplateLiteralLoose(['\n  border-radius: 0px; \n\n margin: 6px 0px;']);
+
 var _templateObject3 = _taggedTemplateLiteralLoose(['\n width: 15%;\n border-radius: 50%; \n']);
 var _templateObject4 = _taggedTemplateLiteralLoose(['\n  \n']);
 
@@ -20,7 +21,7 @@ var videoTime = styled.span(_templateObject4);
 
 
 
-// var Button = styled.div(_templateObject2);
+var Button = styled.div(_templateObject2);
 
 function TextEditor(props) {
 
